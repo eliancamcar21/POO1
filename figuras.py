3156f7f86e188621bb9d5d9740ccef4d94119dfe
eliancamcar21 <eliancamaca21@gmail.com>
2025-02-05 10:34:07 -0600

    update figuras.py

--- figuras.py
+++ figuras.py
@@ -1,27 +1,29 @@
-from math import sqrt
-from math import pi
-from math import tan
+from math import sqrt, pi, tan
 
-def calculoPerimetro(numLados, lado):
-    perimetro = numLados * lado
-    return perimetro
+class Figura:
+    def __init__(self, num_lados, lado):
+        self.num_lados = num_lados
+        self.lado = lado
+        self.perimetro = self.calculo_perimetro()
+        self.area = self.calculo_area()
 
-def calculoArea(numLados, lado,):
-    theta = pi / numLados
-    apotema = lado / (2 * tan(theta))
-    area = (perimetro * apotema)/2
-    return area
+    def calculo_perimetro(self):
+        return self.num_lados * self.lado
 
-def mostrarResultado(numLados, perimetro, area):
-    if numLados <= 1:
-        print("No existen poligonos de un solo lado")
-    else:
-        print("El perímetro de una figura de",numLados,"lados es igual a",perimetro,"y el área es igual a",area,"unidades")
+    def calculo_area(self):
+        theta = pi / self.num_lados
+        apotema = self.lado / (2 * tan(theta))
+        area = (self.perimetro * apotema) / 2
+        return area
 
-numLados = int(input("Ingrese el número de lados de la figura: "))
+    def mostrar_resultado(self):
+        if self.num_lados <= 1:
+            print("No existen poligonos de un solo lado")
+        else:
+            print(f"El perímetro de una figura de {self.num_lados} lados es igual a {self.perimetro} y el área es igual a {self.area} unidades")
+
+num_lados = int(input("Ingrese el número de lados de la figura: "))
 lado = float(input("Ingrese el tamaño del lado: "))
 
-perimetro = calculoPerimetro(numLados, lado)
-area = calculoArea(numLados, lado)
-
-mostrarResultado(numLados, perimetro, area)
+poligono = Figura(num_lados, lado)
+poligono.mostrar_resultado()
